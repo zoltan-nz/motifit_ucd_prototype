4595d01cf2621d508229993283090c30f269252a
Zoltan <zdebre@gmail.com>
2013-07-29 23:11:36 +0100

    Generating new rails 4.0 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MotifitUcdPrototype::Application.config.secret_key_base = '3b85bf4bcfa1ae1752fa476758a56be2da36c3d02d5fcbf6eedefa660ac22e049834f45ef2116155bb728752630509cfbaccf60bece9e5acd4b9360dfc2759b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MotifitUcdPrototype::Application.config.session_store :cookie_store, key: '_motifit_ucd_prototype_session'
