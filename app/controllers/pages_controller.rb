efd3fe90cfd9acf1c5f26d2cf5a6bcc895c30464
Zoltan <zdebre@gmail.com>
2013-07-30 00:38:50 +0100

    pages created, turbolinks deleted

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def signup
+  end
+
+  def meetups
+  end
+
+  def profile
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 MotifitUcdPrototype::Application.routes.draw do
+  root "pages#index"
+  get "pages/signup"
+  get "pages/meetups"
+  get "pages/profile"
+  get "pages/about"
+  get "pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
